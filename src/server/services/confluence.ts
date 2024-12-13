fa40619e00bfe2de09e0632c8b9dfec71567bbd3
S.Fedorov <s.fedorov@example.com>
2024-12-13 18:25:48 +0500

    Add HTTP request method to Confluence service

--- src/server/services/confluence.ts
+++ src/server/services/confluence.ts
@@ -20,4 +20,34 @@ export class ConfluenceService {
             .map(cookie => `${cookie.name}=${cookie.value}`)
             .join('; ');
     }
+
+    private async request<T>(path: string, options: RequestInit = {}): Promise<T> {
+        const url = `${this.baseUrl}${this.apiPath}${path}`;
+        
+        const response = await fetch(url, {
+            ...options,
+            headers: {
+                ...options.headers,
+                'Cookie': this.cookieHeader,
+                'Accept': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            const error: ConfluenceError = {
+                statusCode: response.status,
+                message: response.statusText
+            };
+
+            try {
+                error.data = await response.json();
+            } catch {
+                // Ignore JSON parse errors
+            }
+
+            throw error;
+        }
+
+        return response.json();
+    }
 }
